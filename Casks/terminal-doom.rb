80b595af432b7649f7951cd9ca481e073622f520
Adam Zafir <me@adamzafir.com>
2026-07-27 13:27:19 +0800

    chore: update Homebrew cask to v0.1.2

--- Casks/terminal-doom.rb
+++ Casks/terminal-doom.rb
@@ -1,9 +1,9 @@
 cask "terminal-doom" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.1"
-  sha256 arm:   "f50e1fb081cc6991a45a26fd4b529f117196ecc6c993ea91ee623398ee5ac9e0",
-         intel: "f6253dc6dae2abf043f5b4a51ed2342897cae2b7c69a0da84197129afb4c04eb"
+  version "0.1.2"
+  sha256 arm:   "618d84456ea46c37c39987de91d647034c6375b11023db0575e69d2cb7111507",
+         intel: "bd555f8973e3bf7fc2b4f400c4fa33daaa776902b7336fd113737185ca4afd92"
 
   url "https://github.com/adamzafir/terminal-doom/releases/download/v#{version}/terminal-doom-#{arch}-apple-darwin.tar.gz"
   name "Terminal Doom"
